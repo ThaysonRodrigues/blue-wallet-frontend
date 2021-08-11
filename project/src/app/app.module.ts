1720956d2988302331c395051f99721d4631a37a
Thayson Rodrigues de Souza <contato.thayson@gmail.com>
2021-08-11 01:17:58 -0300

    Adicionando tela de login

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +13,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]

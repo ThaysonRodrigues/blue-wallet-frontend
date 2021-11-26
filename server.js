74f1079b54bb3329df8b1ddfa4e5dc7091aa2aed
Thayson Rodrigues de Souza <contato.thayson@gmail.com>
2021-11-25 21:36:45 -0300

    teste integração

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const { createProxyMiddleware} = require('http-proxy-middleware');
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/dist/blue-wallet'));
+//app.use(express.static(__dirname + '/dist/blue-wallet'));
+
+app.use('/api', createProxyMiddleware({ target: 'https://sboot-blue-wallet.herokuapp.com', changeOrigin: true }));
 
 app.get('/*', (req, res) => {
     res.sendFile(__dirname + '/dist/blue-wallet/index.html');

1da68d2c916ae93c1f4193ee4b26a378793eecce
Thayson Rodrigues de Souza <contato.thayson@gmail.com>
2021-11-27 15:01:55 -0300

    teste integração

--- server.js
+++ server.js
@@ -1,30 +1,9 @@
 const express = require('express');
-const http = require('http');
-const bodyParser = require('body-parser');
-var cors = require('cors');
-
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-var corsOptions = {
-    origin: 'https://sboot-blue-wallet.herokuapp.com/',
-    optionsSuccessStatus: 200
- }
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-   extended: false
-}));
-
 app.use(express.static(__dirname + '/dist/blue-wallet'));
-
-app.get('/webapi', cors(corsOptions), function(req, res, next) {
-    res.json({
-       msg: 'This is CORS-enabled for Java API!'
-    })
- });
-
 
 app.get('/*', (req, res) => {
     res.sendFile(__dirname + '/dist/blue-wallet/index.html');
